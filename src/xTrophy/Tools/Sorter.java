6268fe9218355fe655e73099c913d28e0c318d14
Antony Torres <mr.owlpuff@gmail.com>
2012-08-06 23:10:51 -0500

    Derp fix
    Had my sorter backwards lol

--- src/xTrophy/Tools/Sorter.java
+++ src/xTrophy/Tools/Sorter.java
@@ -9,9 +9,9 @@ public class Sorter implements Comparator<String>{
 		    String[] s2  = o2.split("(§§)");
 		    
 		    if (Integer.parseInt(s1[1]) < Integer.parseInt(s2[1])) {
-		        return -1;
-		      } else if (Integer.parseInt(s1[1]) > Integer.parseInt(s2[1])) {
 		        return 1;
+		      } else if (Integer.parseInt(s1[1]) > Integer.parseInt(s2[1])) {
+		        return -1;
 		      } else {
 		        return 0;
 		      }
